cbc0b32cc09cd5b2ff1e24fec59d36a3418694ff
SupawatN <tortoon00@hotmail.com>
2019-07-30 15:18:04 +0700

    adding files

--- config.inc.php
+++ config.inc.php
@@ -78,4 +78,12 @@ $db_persist = FALSE;
    to change the default configuration. Do _NOT_ modify systemdefaults.inc.php
    or areadefaults.inc.php.  */
 
-$auth["type"] = "none";
+//$auth["type"] = "none";
+
+$auth["admin"][] = "admin";
+
+$auth["session"] = "php";
+
+$auth["type"] = "config";
+
+$auth["user"]["USERNAME"] = "thisisabook";
